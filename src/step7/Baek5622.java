809515b1b42a5495585c06984f5da36877d90b46
BuildTools <parkjinsu4755@daum.net>
2019-09-21 18:51:10 +0900

    0921

---
+++ src/step7/Baek5622.java
@@ -0,0 +1,61 @@
+package step7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek5622 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String phoneNum = br.readLine();
+        br.close();
+        int time = 0;
+        for (int i = 0; i < phoneNum.length(); i++) {
+            char numChar = phoneNum.charAt(i);
+            time += numTime(numChar);
+        }
+
+        System.out.println(time);
+    }
+
+    private static int numTime(char num) {
+        switch (num) {
+            case 'A':
+            case 'B':
+            case 'C':
+                return 3;
+            case 'D':
+            case 'E':
+            case 'F':
+                return 4;
+            case 'G':
+            case 'H':
+            case 'I':
+                return 5;
+            case 'J':
+            case 'K':
+            case 'L':
+                return 6;
+            case 'M':
+            case 'N':
+            case 'O':
+                return 7;
+            case 'P':
+            case 'Q':
+            case 'R':
+            case 'S':
+                return 8;
+            case 'T':
+            case 'U':
+            case 'V':
+                return 9;
+            case 'W':
+            case 'X':
+            case 'Y':
+            case 'Z':
+                return 10;
+            default:
+                return -1;
+        }
+    }
+}
