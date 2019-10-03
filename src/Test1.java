c7fc7954f7954fc376a892f533f336324951d92e
jinsu Park <parkjinsu4755@daum.net>
2019-10-03 17:07:12 +0900

    1003-1

--- src/Test1.java
+++
@@ -1,9 +0,0 @@
-public class Test1 {
-    public static void main(String[] args) {
-        String en = "hello this is english";
-        String ko = "안녕 이건 한글";
-
-        System.out.println(en.charAt(0));
-        System.out.println(ko.charAt(0));
-    }
-}
