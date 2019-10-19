73a771c58bf22d3419aff71cb9e0b86c506d9287
BuildTools <parkjinsu4755@daum.net>
2019-10-20 03:22:46 +0900

    ~ing

---
+++ src/Baek1003.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Baek1003 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] arr = new int[n + 1][2];
+        }
+    }
+}
