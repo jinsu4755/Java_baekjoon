3c86596e4bacb969dc44a5ce6213c59276eb9775
BuildTools <parkjinsu4755@daum.net>
2019-11-11 02:55:16 +0900

    1111

---
+++ src/Baek2558.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Baek2558 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numA = sc.nextInt();
+        int numB = sc.nextInt();
+
+        System.out.println(numA + numB);
+    }
+}
