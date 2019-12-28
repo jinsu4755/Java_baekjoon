386836cd450bdb6b02650c78280e9a7c64692a76
jinsu4755 <parkjinsu4755@daum.net>
2019-12-28 18:59:33 +0900

    Baek

---
+++ src/Baek5218.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Baek5218 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 0; i < N; i++) {
+            String[] line = sc.nextLine().split(" ");
+
+        }
+    }
+}
